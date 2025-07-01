2c9b909b88b6556c59bb20a23ff670293eb82315
Ryoya Otomo <s2212214@st.tohtech.ac.jp>
2025-07-01 12:44:48 +0900

    フォント設定の追加

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use eframe::egui;
+use eframe::egui::{self, FontData, FontDefinitions};
 
 #[derive(Default)]
 struct MomijiEditor;
@@ -7,6 +7,32 @@ impl eframe::App for MomijiEditor {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
         
     }
+}
+
+fn setup_custom_fonts(ctx: &egui::Context) {
+    // フォント設定の取得
+    let mut fonts = FontDefinitions::default();
+
+    // 日本語フォント(可変ウェイト)を追加
+    fonts.font_data.insert(
+        "noto_sans_jp".to_owned(),
+        FontData::from_static(include_bytes!("../assets/NotoSansJP.ttf")).into()
+    );
+
+    // フォントファミリーに追加
+    fonts
+        .families
+        .entry(egui::FontFamily::Proportional)
+        .or_default()
+        .insert(0, "noto_sans_jp".to_owned());
+
+    fonts
+        .families
+        .entry(egui::FontFamily::Monospace)
+        .or_default()
+        .push("noto_sans_jp".to_owned());
+
+    ctx.set_fonts(fonts);
 }
 
 fn main() {
@@ -18,10 +44,11 @@ fn main() {
     };
 
     // アプリの実行
-    eframe::run_native(
+    let _ = eframe::run_native(
         "momiji-editor",
         options,
-        Box::new(|_cc| {
+        Box::new(|cc| {
+            setup_custom_fonts(&cc.egui_ctx);
             Ok(Box::new(MomijiEditor::default()))
         }),
     );
