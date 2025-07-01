69cbcb2e1239416804eaed577b04ff741fbb8689
ryoya <sateraito3696@gmail.com>
2025-07-01 12:26:56 +0900

    フォントファイルの追加とwindow表示処理の追加

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use eframe::egui;
+
+#[derive(Default)]
+struct MomijiEditor;
+
+impl eframe::App for MomijiEditor {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // windowの設定
+    let options = eframe::NativeOptions {
+        viewport: egui::viewport::ViewportBuilder::default()
+            .with_inner_size([600.0, 800.0]),
+        ..Default::default()
+    };
+
+    // アプリの実行
+    eframe::run_native(
+        "momiji-editor",
+        options,
+        Box::new(|_cc| {
+            Ok(Box::new(MomijiEditor::default()))
+        }),
+    );
 }
